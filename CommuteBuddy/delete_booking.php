04529a983d1f7e695060fa35a1093149585a76f8
cerealciel <clairegernade@gmail.com>
2026-03-24 17:27:34 +0800

    Delete CommuteBuddy directory

--- CommuteBuddy/delete_booking.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'db.php';
-
-$id = $_POST['booking_id'];
-
-$conn->query("DELETE FROM bookings WHERE booking_id = $id");
-
-header("Location: index.php");
--- CommuteBuddy/delete_ride.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'db.php';
-
-$id = $_POST['ride_id'];
-
-$conn->query("DELETE FROM bookings WHERE ride_id = $id");
-$conn->query("DELETE FROM rides WHERE ride_id = $id");
-
-header("Location: index.php");
